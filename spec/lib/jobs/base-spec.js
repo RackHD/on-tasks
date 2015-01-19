30fd5bb6441b605cfcaeb188b4803b7b60a5d7c2
Joe Heck <heckj@mac.com>
2015-01-18 18:12:20 -0800

    yeah, missing files

---
+++ spec/lib/jobs/base-spec.js
@@ -0,0 +1,36 @@
+// Copyright 2014, Renasar Technologies Inc.
+/* jshint node: true */
+
+'use strict';
+
+module.exports = {
+    before: function (callback) {
+        before(function () {
+            callback(this);
+        });
+    },
+    examples: function () {
+        before(function () {
+            expect(this.Jobclass).to.be.ok;
+            expect(this.Jobclass).to.be.an.function;
+        });
+
+        describe('Instance Methods', function() {
+            
+            it('members should have a run function', function() {
+                expect(this.Jobclass).to.respondTo('run');
+            });
+
+            it('members should have a cancel function', function() {
+                expect(this.Jobclass).to.respondTo('cancel');
+            });
+
+        });
+
+        describe('Class Methods', function () {
+            it('should have a create static method', function() {
+                expect(this.Jobclass).itself.to.respondTo('create');
+            });
+        });
+    }
+};
