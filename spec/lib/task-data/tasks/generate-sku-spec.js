8b4cfafcf5b59b308a96eefede90525f91fe82e3
Jason Gardino <jason.gardino@renasar.com>
2015-02-02 12:43:45 -0800

    adding tasks for SKU generation

---
+++ spec/lib/task-data/tasks/generate-sku-spec.js
@@ -0,0 +1,17 @@
+// Copyright 2015, Renasar Technologies Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(__filename, function () {
+    var base = require('./base-tasks-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/task-data/tasks/generate-sku.js');
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
