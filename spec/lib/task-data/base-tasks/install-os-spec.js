6a46b9ec405e00ba3e30b9fcbf939eb9f4d2df7a
Tracy <tracy.davies@renasar.com>
2015-02-02 17:37:41 -0800

    fixed describe(__filename to describe(require('path').basename(__filename)

--- spec/lib/task-data/base-tasks/install-os-spec.js
+++ spec/lib/task-data/base-tasks/install-os-spec.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-describe(__filename, function () {
+describe(require('path').basename(__filename), function () {
     var base = require('./base-task-data-spec');
 
     base.before(function (context) {
