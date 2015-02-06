50989ecdf95893ccf6705e681d4ab037c4e4f9cc
Ben Broderick Phillips <ben@benbp.net>
2015-02-05 16:16:03 -0800

    Initial port/add of SNMP job and tooling

---
+++ spec/lib/jobs/snmp-job-spec.js
@@ -0,0 +1,71 @@
+// Copyright 2015, Renasar Technologies Inc.
+/* jshint node:true */
+
+'use strict';
+
+var uuid = require('node-uuid');
+var _ = require('lodash');
+
+describe(require('path').basename(__filename), function () {
+    var injector;
+    var base = require('./base-spec');
+
+    base.before(function (context) {
+        var _ = helper.baseInjector.get('_');
+        // create a child injector with renasar-core and the base pieces we need to test this
+        injector = helper.baseInjector.createChild(_.flatten([
+            helper.require('/spec/mocks/logger.js'),
+            helper.requireGlob('/lib/services/*.js'),
+            helper.require('/lib/utils/job-utils/net-snmp-tool.js'),
+            helper.require('/lib/utils/job-utils/net-snmp-parser.js'),
+            helper.require('/lib/jobs/base-job.js'),
+            helper.require('/lib/jobs/snmp-job.js')
+        ]));
+
+        context.Jobclass = injector.get('Job.Snmp');
+    });
+
+    describe('Base', function () {
+        base.examples();
+    });
+
+    describe("snmp-job", function() {
+        beforeEach(function() {
+            this.snmp = new this.Jobclass({ snmpRoutingKey: uuid.v4() }, {}, uuid.v4());
+        });
+
+        it("should have a _run() method", function() {
+            expect(this.snmp).to.have.property('_run').with.length(0);
+        });
+
+        it("should have a snmp command subscribe method", function() {
+            expect(this.snmp).to.have.property('_subscribeRunSnmpCommand').with.length(1);
+        });
+
+        it("should listen for snmp command requests", function(done) {
+            var self = this;
+            self.snmp.collectHostSnmp = sinon.promise();
+            self.snmp._publishSnmpResult = sinon.stub();
+            self.snmp._subscribeRunSnmpCommand = function(callback) {
+                self.snmp.on('test-subscribe-snmp-command', function(machine) {
+                    callback(machine);
+                });
+            };
+
+            self.snmp._run();
+
+            _.forEach(_.range(100), function() {
+                self.snmp.emit('test-subscribe-snmp-command');
+            });
+
+            process.nextTick(function() {
+                try {
+                    expect(self.snmp.collectHostSnmp.callCount).to.equal(100);
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+        });
+    });
+});
