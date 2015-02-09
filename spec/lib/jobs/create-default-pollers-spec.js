b0aff29cfdffc4bc07c409d955b5113309e7a25e
Jason Gardino <jason.gardino@renasar.com>
2015-02-09 15:01:22 -0800

    adding check for bmc catalog to create default pollers job

--- spec/lib/jobs/create-default-pollers-spec.js
+++ spec/lib/jobs/create-default-pollers-spec.js
@@ -30,6 +30,11 @@ describe("Job.Pollers.CreateDefault", function () {
         waterline.workitems = {
             createIpmiPollers: sinon.stub().returns(Q.resolve())
         };
+        waterline.catalogs = {
+            findMostRecent: sinon.stub().returns(Q.resolve({
+                id: 'bc7dab7e8fb7d6abf8e7d6a1'
+            }))
+        };
     });
 
     it('should create pollers for a job with options.nodeId', function (done) {
@@ -37,9 +42,23 @@ describe("Job.Pollers.CreateDefault", function () {
 
         var job = new CleanWorkItems({ nodeId: nodeId }, { graphId: uuid.v4() }, uuid.v4());
         job.on('done', function (err) {
-            expect(waterline.workitems.createIpmiPollers).to.have.been.calledOnce;
-            expect(waterline.workitems.createIpmiPollers).to.have.been.calledWith(nodeId);
-            done(err);
+            if (err) {
+                done(err);
+                return;
+            }
+            try {
+                expect(waterline.catalogs.findMostRecent).to.have.been.calledOnce;
+                expect(waterline.catalogs.findMostRecent.firstCall.args[0])
+                    .to.have.property('node', nodeId);
+                expect(waterline.catalogs.findMostRecent.firstCall.args[0])
+                    .to.have.property('source', 'bmc');
+                expect(waterline.workitems.createIpmiPollers).to.have.been.calledOnce;
+                expect(waterline.workitems.createIpmiPollers).to.have.been.calledWith(nodeId);
+            } catch (e) {
+                done(e);
+                return;
+            }
+            done();
         });
 
         job.run();
@@ -50,9 +69,50 @@ describe("Job.Pollers.CreateDefault", function () {
 
         var job = new CleanWorkItems({}, { target: nodeId, graphId: uuid.v4() }, uuid.v4());
         job.on('done', function (err) {
-            expect(waterline.workitems.createIpmiPollers).to.have.been.calledOnce;
-            expect(waterline.workitems.createIpmiPollers).to.have.been.calledWith(nodeId);
-            done(err);
+            if (err) {
+                done(err);
+                return;
+            }
+            try {
+                expect(waterline.catalogs.findMostRecent).to.have.been.calledOnce;
+                expect(waterline.catalogs.findMostRecent.firstCall.args[0])
+                    .to.have.property('node', nodeId);
+                expect(waterline.catalogs.findMostRecent.firstCall.args[0])
+                    .to.have.property('source', 'bmc');
+                expect(waterline.workitems.createIpmiPollers).to.have.been.calledOnce;
+                expect(waterline.workitems.createIpmiPollers).to.have.been.calledWith(nodeId);
+            } catch (e) {
+                done(e);
+                return;
+            }
+            done();
+        });
+
+        job.run();
+    });
+
+    it('should not create pollers when bmc catalog does not exist', function (done) {
+        var nodeId = 'bc7dab7e8fb7d6abf8e7d6af';
+
+        waterline.catalogs.findMostRecent.returns(Q.resolve());
+        var job = new CleanWorkItems({}, { target: nodeId, graphId: uuid.v4() }, uuid.v4());
+        job.on('done', function (err) {
+            if (err) {
+                done(err);
+                return;
+            }
+            try {
+                expect(waterline.catalogs.findMostRecent).to.have.been.calledOnce;
+                expect(waterline.catalogs.findMostRecent.firstCall.args[0])
+                    .to.have.property('node', nodeId);
+                expect(waterline.catalogs.findMostRecent.firstCall.args[0])
+                    .to.have.property('source', 'bmc');
+                expect(waterline.workitems.createIpmiPollers).to.not.have.been.called;
+            } catch (e) {
+                done(e);
+                return;
+            }
+            done();
         });
 
         job.run();
