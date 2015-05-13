e0593d1199ca971badddb9e5929cc499e221c204
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2015-05-13 14:26:42 -0700

    Support mustache-style templating in task definitions

--- spec/lib/task-spec.js
+++ spec/lib/task-spec.js
@@ -53,6 +53,14 @@ describe("Task", function () {
         noopDefinition = _.merge(noopTask, baseNoopTask);
     });
 
+    beforeEach('task-spec beforeEach', function() {
+        this.sandbox = sinon.sandbox.create();
+    });
+
+    afterEach('task-spec beforeEach', function() {
+        this.sandbox.restore();
+    });
+
     it("should create subscriptions on start", function() {
         var task = Task.create(noopDefinition, {}, {});
         var taskProtocol = helper.injector.get('Protocol.Task');
@@ -63,6 +71,146 @@ describe("Task", function () {
         .then(function() {
             expect(task.subscriptions.run).to.equal('test run subscription');
             expect(task.subscriptions.cancel).to.equal('test cancel subscription');
+        });
+    });
+
+    describe("option rendering", function() {
+        var definition;
+
+        before(function() {
+            definition = _.cloneDeep(noopDefinition);
+        });
+
+        beforeEach(function() {
+            definition.options = null;
+        });
+
+        it("should render definition options", function() {
+            definition.options = {
+                testRenderVal: 'test rendered',
+                toRenderVal: 'val: {{ options.testRenderVal }}'
+            };
+            var task = Task.create(definition, {}, {});
+            expect(task.options.toRenderVal).to.equal('val: ' + definition.options.testRenderVal);
+        });
+
+        it("should render options using the '|' helper encapsulated by spaces", function() {
+            definition.options = {
+                toRenderVal: 'val: {{ options.doesNotExist | DEFAULT }}'
+            };
+            var task = Task.create(definition, {}, {});
+            expect(task.options.toRenderVal).to.equal('val: DEFAULT');
+        });
+
+        it("should render options using the '|' helper not encapsulated by spaces", function() {
+            definition.options = {
+                toRenderVal: 'val: {{ options.doesNotExist|DEFAULT }}'
+            };
+            var task = Task.create(definition, {}, {});
+            expect(task.options.toRenderVal).to.equal('val: DEFAULT');
+        });
+
+        it("should render options with multiple '|' helpers", function() {
+            definition.options = {
+                toRenderVal: 'val: {{ options.doesNotExist | options.stillNotThere | DEFAULT }}'
+            };
+            var task = Task.create(definition, {}, {});
+            expect(task.options.toRenderVal).to.equal('val: DEFAULT');
+        });
+
+        it("should render values from a nested option definition", function() {
+            definition.options = {
+                renderOptions: {
+                    testRenderVal: 'test rendered'
+                },
+                toRenderVal: 'val: {{ options.renderOptions.testRenderVal }}'
+            };
+            var task = Task.create(definition, {}, {});
+            expect(task.options.toRenderVal)
+                .to.equal('val: ' + definition.options.renderOptions.testRenderVal);
+        });
+
+        it("should render values from an array", function() {
+            definition.options = {
+                testRenderVal1: 'test rendered 1',
+                testRenderVal2: 'test rendered 2',
+                toRenderVal: [
+                    'val1: {{ options.testRenderVal1 }}',
+                    'val2: {{ options.testRenderVal2 }}'
+                ]
+            };
+            var task = Task.create(definition, {}, {});
+            expect(task.options.toRenderVal).to.deep.equal([
+                'val1: ' + definition.options.testRenderVal1,
+                'val2: ' + definition.options.testRenderVal2
+            ]);
+        });
+
+        it("should render values within a nested option definition", function() {
+            definition.options = {
+                testRenderVal1: 'test rendered 1',
+                testRenderVal2: 'test rendered 2',
+                toRenderObject: {
+                    toRenderArray: [
+                        'val1: {{ options.testRenderVal1 }}',
+                        'val2: {{ options.testRenderVal2 }}'
+                    ],
+                    toRenderVal: {
+                        toRenderValNested: '{{ options.testRenderVal1 }}'
+                    }
+                }
+            };
+            var task = Task.create(definition, {}, {});
+            expect(task.options.toRenderObject.toRenderArray).to.deep.equal([
+                'val1: ' + definition.options.testRenderVal1,
+                'val2: ' + definition.options.testRenderVal2
+            ]);
+            expect(task.options.toRenderObject.toRenderVal.toRenderValNested)
+                .to.equal(definition.options.testRenderVal1);
+        });
+
+        it("should render own instance values", function() {
+            definition.options = {
+                instanceId: '{{ task.instanceId }}',
+                nodeId: '{{ task.nodeId }}'
+            };
+            var task = Task.create(definition, {}, { target: 'testnodeid' });
+            expect(task.options.instanceId).to.be.ok.and.to.equal(task.instanceId);
+            expect(task.options.nodeId).to.be.ok.and.to.equal(task.nodeId);
+        });
+
+        it("should render api and server values", function() {
+            var config = this.sandbox.stub(helper.injector.get('Services.Configuration'));
+            config.get.withArgs('server').returns('10.1.1.1');
+            config.get.withArgs('httpPort').returns('80');
+            config.get.withArgs('httpsPort').returns('443');
+            config.getAll.returns({
+                testConfigValue: 'test config value'
+            });
+
+            var baseHttpRoute = 'http://' + config.get('server') + ':' + config.get('httpPort');
+            var baseHttpsRoute = 'https://' + config.get('server') + ':' + config.get('httpsPort');
+            definition.options = {
+                filesRoute: '{{ api.baseHttpRoute }}/files',
+                filesRouteHttps: '{{ api.baseHttpsRoute }}/files',
+                testConfigValue: 'test: {{ server.testConfigValue }}'
+            };
+            var task = Task.create(definition, {}, {});
+
+            expect(task.options.filesRoute).to.equal(baseHttpRoute + '/files');
+            expect(task.options.filesRouteHttps).to.equal(baseHttpsRoute + '/files');
+            expect(task.options.testConfigValue)
+                .to.equal('test: ' + config.getAll().testConfigValue);
+        });
+
+        it("should throw an error if the render key does not exist in context", function() {
+            var TemplateRenderError = helper.injector.get('TemplateRenderer').TemplateRenderError;
+            definition.options = {
+                nonExistantValue: '{{ options.doesNotExist }}'
+            };
+            expect(function() {
+                Task.create(definition, {}, {});
+            }).to.throw(TemplateRenderError);
         });
     });
 
