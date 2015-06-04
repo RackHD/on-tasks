530e13b187e9245ab5f8aeed2538563b88516c1a
Ben Broderick Phillips <ben.broderickphillips@emc.com>
2015-06-04 16:59:36 -0700

    Add IPMI mc reset cold functionality to obm service, add task

---
+++ spec/lib/task-data/tasks/mc-reset-cold-spec.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2015, EMC Corporation
+/* jshint node:true */
+
+'use strict';
+
+describe('MC Reset Cold', function () {
+    var base = require('./base-tasks-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/task-data/tasks/mc-reset-cold.js');
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
