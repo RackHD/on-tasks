f9436d27450a91da106051d771646cf775a2999a
Glenn <glennt3@usxxglennt3m1.corp.emc.com>
2015-07-22 15:48:27 -0700

    task tests

---
+++ spec/lib/task-data/base-tasks/snmp-collect-spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-task-data-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/task-data/base-tasks/snmp-collect.js');
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
