76b486cb0def5138a34a329fa0a220bdb837a9ef
Lin, Lyne <lyne.lin@emc.com>
2015-07-30 07:39:13 -0400

    Added mgmt server discovery job and tasks for MAG-82

---
+++ spec/lib/task-data/base-tasks/ipmi-cataloger-spec.js
@@ -0,0 +1,17 @@
+// Copyright 2015, Renasar Technologies Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-task-data-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/task-data/base-tasks/ipmi-cataloger.js');
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
