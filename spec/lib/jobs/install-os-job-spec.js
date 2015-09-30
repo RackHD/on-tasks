4f2d2fac09090d8069026b286c8ac1d9902ee3eb
Felix Yuan <felix.yuan@emc.com>
2015-09-30 23:17:19 +0800

    Refine parameterize bootstrap code according to review comment

--- spec/lib/jobs/install-os-job-spec.js
+++ spec/lib/jobs/install-os-job-spec.js
@@ -34,7 +34,27 @@ describe('Install OS Job', function () {
         subscribeRequestPropertiesStub.reset();
         subscribeHttpResponseStub.reset();
         job = new InstallOsJob(
-            { profile: 'testprofile', completionUri: '' }, { target: 'testid' }, uuid.v4());
+            {
+                profile: 'testprofile',
+                completionUri: '',
+                version: '7.0',
+                repo: 'http://127.0.0.1:8080/myrepo/7.0/x86_64',
+                rootPassword: 'rackhd',
+                rootSshKey: null,
+                users: [
+                    {
+                        name: 'test',
+                        password: 'testPassword',
+                        uid: 100,
+                        sshKey: ''
+                    }
+                ],
+                dnsServers: null
+            },
+            {
+                target: 'testid'
+            },
+            uuid.v4());
     });
 
     after(function() {
@@ -49,6 +69,20 @@ describe('Install OS Job', function () {
 
     it("should have a profile value", function() {
         expect(job.profile).to.equal('testprofile');
+    });
+
+    it("should generate correct password", function() {
+        expect(job.options.rootEncryptedPassword).to.match(/^\$6\$*\$*/);
+        expect(job.options.users[0].encryptedPassword).to.match(/^\$6\$*\$*/);
+    });
+
+    it("should remove empty ssh key", function() {
+        expect(job.options).to.not.have.property('rootSshKey');
+        expect(job.options.users[0]).to.not.have.property('sshKey');
+    });
+
+    it("should convert some option to empty array", function() {
+        expect(job.options.dnsServers).to.have.length(0);
     });
 
     it("should set up message subscribers", function() {
