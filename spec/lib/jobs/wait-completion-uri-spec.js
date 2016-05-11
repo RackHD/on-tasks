ab12abf526da4c9580aa6e43d8343ef16e1dc894
Zack Young <Zachary.Young@emc.com>
2016-05-11 17:44:59 -0400

    Add task to wait for a completion Uri trigger
    Add a task to wait for a Uri trigger to resolve an issue
    with ESXi installation early completion.

---
+++ spec/lib/jobs/wait-completion-uri-spec.js
@@ -0,0 +1,33 @@
+// Copyright 2016, EMC, Inc.
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var uuid = require('node-uuid'),
+        graphId = uuid.v4(),
+        WaitCompletionJob;
+    var subscribeRequestPropertiesStub;
+    var subscribeHttpResponseStub;
+
+    before(function() { 
+        helper.setupInjector([
+            helper.require('/lib/jobs/base-job.js'),
+            helper.require('/lib/jobs/wait-completion-uri.js'),
+        ]);
+        WaitCompletionJob = helper.injector.get('Job.Wait.Completion.Uri');
+        subscribeRequestPropertiesStub = sinon.stub(
+            WaitCompletionJob.prototype, '_subscribeRequestProperties');
+        subscribeHttpResponseStub = sinon.stub(
+            WaitCompletionJob.prototype, '_subscribeHttpResponse', function(cb) {
+                cb({statusCode: 200, url: 'completion'});
+        });
+    });
+
+    it("should run", function() {
+        var job = new WaitCompletionJob({completionUri: 'completion'}, {}, graphId);
+        job._run().then(function() {
+            expect(subscribeRequestPropertiesStub).to.have.been.called;
+            expect(subscribeHttpResponseStub).to.have.been.called;
+        });
+    });
+});
