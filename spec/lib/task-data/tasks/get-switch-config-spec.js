06294a822cb94832a2f74820e2ed9fce1c3ce43d
Massarrah Tannous <massarrah.tannous@emc.com>
2017-11-02 15:37:57 -0400

    Added tasks and tests: get switch config get switch version update switch firmware on-network login
    Signed-off-by: Bala Balakumaran <bala.balakumaran@dell.com>

---
+++ spec/lib/task-data/tasks/get-switch-config-spec.js
@@ -0,0 +1,18 @@
+// Copyright 2017, Dell EMC, Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-tasks-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require(
+            '/lib/task-data/tasks/get-switch-config.js');
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
