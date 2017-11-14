b3c81971a087229aad90a6b5a23302bf299cd735
HQebupt <qiang.huang1991@gmail.com>
2017-11-13 21:35:03 -0700

    Implement UCS catalog job with new async ucs-service API

--- spec/lib/jobs/ucs-catalog-spec.js
+++ spec/lib/jobs/ucs-catalog-spec.js
@@ -10,8 +10,45 @@ describe('Ucs Catalog Job', function () {
         ucsJob,
         waterline = {},
         sandbox = sinon.sandbox.create();
-        var setup = sandbox.stub().resolves();
-        var clientRequest = sandbox.stub();
+        var nodeChassis = {
+            "autoDiscover": false,
+            "catalogs": "/api/2.0/nodes/5a09300a2ae51d82238fcb73/catalogs",
+            "id": "5a09300a2ae51d82238fcb73",
+            "identifiers": [
+                "1*.1*.1*.1*:sys/chassis-3"
+            ],
+            "name": "sys/chassis-3",
+            "obms": [
+                {
+                    "service": "ucs-obm-service",
+                    "ref": "/api/2.0/obms/5a09300a2ae51d82238fcb74"
+                }
+            ],
+            "tags": "/api/2.0/nodes/5a09300a2ae51d82238fcb73/tags",
+            "pollers": "/api/2.0/nodes/5a09300a2ae51d82238fcb73/pollers",
+            "relations": [],
+            "type": "enclosure",
+            "workflows": "/api/2.0/nodes/5a09300a2ae51d82238fcb73/workflows",
+            "ibms": [],
+            "obmSettings": [
+                {
+                    "service": "ucs-obm-service",
+                    "config": {
+                        "uri": "https://1*.1*.1*.1*:7080",
+                        "host": "1*.1*.1*.1*",
+                        "root": "",
+                        "port": "7080",
+                        "protocol": "https",
+                        "ucsUser": "ucspe",
+                        "ucsHost": "1*.1*.1*.1*",
+                        "verifySSL": false,
+                        "dn": "sys/chassis-3"
+                    }
+
+                }
+
+            ]
+        };
         var node = {
             "autoDiscover": false,
             "createdAt": "2017-01-26T19:25:50.337Z",
@@ -23,14 +60,14 @@ describe('Ucs Catalog Job', function () {
             "obm": [
                 {
                     "config": {
-                        "uri": "http://10.240.19.226:6080/sys",
-                        "host": "10.240.19.226",
+                        "uri": "http://1*.1*.1*.1*:6080/sys",
+                        "host": "1*.1*.1*.1*",
                         "root": "/sys",
                         "port": "6080",
                         "protocol": "http",
                         "username": "ucspe",
                         "password": "ucspe",
-                        "ucs": "10.240.19.236",
+                        "ucs": "1*.1*.1*.1*",
                         "verifySSL": false
                     },
                     "service": "ucs-obm-service"
@@ -45,13 +82,13 @@ describe('Ucs Catalog Job', function () {
                 {
                     "service": "ucs-obm-service",
                     "config": {
-                        "uri": "http://10.240.19.226:6080/sys",
-                        "host": "10.240.19.226",
+                        "uri": "http://1*.1*.1*.1*:6080/sys",
+                        "host": "1*.1*.1*.1*",
                         "root": "/sys",
                         "port": "6080",
                         "protocol": "http",
                         "username": "ucspe",
-                        "ucs": "10.240.19.236",
+                        "ucs": "1*.1*.1*.1*",
                         "verifySSL": false
                     }
                 }
@@ -60,26 +97,18 @@ describe('Ucs Catalog Job', function () {
 
 
     before(function() { 
-        function UcsTool() {
-            this.setup = setup;
-            this.clientRequest = clientRequest;
-            this.settings = {
-                root: '/'
-            };
-        }
-        
         helper.setupInjector([
             helper.require('/lib/jobs/base-job.js'),
+            helper.require('/lib/jobs/ucs-base-job.js'),
             helper.require('/lib/jobs/ucs-catalog.js'),
             helper.require('/lib/utils/job-utils/ucs-tool.js'),
             helper.di.simpleWrapper(waterline,'Services.Waterline'),
-            helper.di.simpleWrapper(UcsTool,'JobUtils.UcsTool')
         ]);
         waterline.catalogs = {
             create: sandbox.stub().resolves()
         };
-        waterline.nodes = {
-            getNodeById: sandbox.stub().resolves(node)
+        waterline.obms = {
+            findByNode: sandbox.stub().resolves(node.obmSettings[0])
         };
     });
     
@@ -93,23 +122,26 @@ describe('Ucs Catalog Job', function () {
             uri:'fake',
             username:'user',
             password:'pass'
-        }, {target:'abc'}, graphId);
+        }, {physicalNodeList : ['abc']}, graphId);
         sandbox.stub(ucsJob, "_done");
-        clientRequest.resetBehavior();
-        clientRequest.reset();
+        ucsJob._ucsRequestAsync = sandbox.stub().resolves();
         waterline.catalogs.create.reset();
+        waterline.nodes = {
+            getNodeById: sandbox.stub().resolves(node)
+        };
+
     });
        
     describe('run catalog ucs servers', function() {
         it('should successfully catalog servers', function() {
-            clientRequest.withArgs('/catalog?identifier=sys/rack-unit-2')
-                        .resolves({ "body": sampleData.rackunit});
-            clientRequest.withArgs('/catalog?identifier=sys/rack-unit-2/board')
-                        .resolves({ "body": sampleData.board});
-            clientRequest.withArgs('/catalog?identifier=sys/rack-unit-2/board/memarray-1')
-                        .resolves({ "body": sampleData["memarray-1"]});
-            clientRequest.withArgs('/catalog?identifier=sys/rack-unit-2/board/storage-flexflash-1')
-                        .resolves({ "body": sampleData["storage-flexflash-1"]});
+            ucsJob._ucsRequestAsync.onCall(0)
+                .resolves({ "body": sampleData.rackunit});
+            ucsJob._ucsRequestAsync.onCall(1)
+                .resolves({ "body": sampleData.board});
+            ucsJob._ucsRequestAsync.onCall(2)
+                .resolves({ "body": sampleData["memarray-1"]});
+            ucsJob._ucsRequestAsync.onCall(3)
+                .resolves({ "body": sampleData["storage-flexflash-1"]});
             return ucsJob._run()
             .then(function() {
                 var ucsResult = _.cloneDeep(sampleData.rackunit[3]);
@@ -130,7 +162,7 @@ describe('Ucs Catalog Job', function () {
                 boardResult.children = boardChildren;
                 expect(waterline.nodes.getNodeById).to.be.calledOnce;
                 expect(waterline.nodes.getNodeById).to.be.calledWith('abc');
-                expect(clientRequest.callCount).to.equal(4);
+                expect(ucsJob._ucsRequestAsync.callCount).to.equal(4);
                 expect(waterline.catalogs.create.getCall(0).args[0].node).to.equal('abc');
                 expect(waterline.catalogs.create.getCall(0).args[0].source)
                     .to.equal('UCS:psu-collection');
@@ -143,6 +175,20 @@ describe('Ucs Catalog Job', function () {
                 expect(waterline.catalogs.create.getCall(2).args[0].node).to.equal('abc');
                 expect(waterline.catalogs.create.getCall(2).args[0].source).to.equal('UCS');
                 expect(waterline.catalogs.create.getCall(2).args[0].data).to.deep.equal(ucsResult);
+            });
+        });
+
+       it('should successfully catalog chassis', function() {
+            waterline.nodes.getNodeById = sandbox.stub().resolves(nodeChassis);
+            ucsJob._ucsRequestAsync.onCall(0)
+                .resolves({ "body": sampleData["chassis-3"]});
+            return ucsJob._run()
+            .then(function() {
+                expect(waterline.nodes.getNodeById).to.be.calledOnce;
+                expect(waterline.nodes.getNodeById).to.be.calledWith('abc');
+                expect(ucsJob._ucsRequestAsync.callCount).to.equal(1);
+                expect(waterline.catalogs.create.getCall(0).args[0].source)
+                      .to.equal('UCS');
             });
         });
 
@@ -160,6 +206,16 @@ describe('Ucs Catalog Job', function () {
             ucsJob.catalogServers('123').then(function(nodeId) {
                 expect(nodeId).to.equal('123');
             });
+        });
+
+        it('should log error if no obm server is found.', function() {
+            waterline.obms.findByNode.resolves();
+            return ucsJob._getObmsByNodeId('123')
+                .then(function() {
+                    throw new Error('test should fail.');
+                }, function(error) {
+                    expect(error.message).to.equal('No ucs-obm-service found for id: 123');
+                });
         });
     });
 });
