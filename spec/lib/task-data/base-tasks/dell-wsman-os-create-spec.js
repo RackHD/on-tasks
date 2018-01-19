b4e7e0137371fd6af4e205fb6b769e81f8d202bf
Muhammad, Rahman <Rahman_Muhammad@Dell.com>
2018-01-19 16:47:08 -0600

    Implemented code review comments for OS deployment

---
+++ spec/lib/task-data/base-tasks/dell-wsman-os-create-spec.js
@@ -0,0 +1,19 @@
+// Copyright 2017, Dell EMC, Inc.
+/* jshint node:true */
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-task-data-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require(
+            '/lib/task-data/base-tasks/dell-wsman-os-create-base.js'
+        );
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
