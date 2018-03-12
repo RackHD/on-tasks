9747d5475a1d9d629bfe26cdfe3148215753663a
Norton Luo <norton.luo@emc.com>
2018-03-12 09:10:46 +0000

    Add task to update wsman discovered node mac
    Update copyrights

---
+++ spec/lib/task-data/base-tasks/wsman-update-lookups-spec.js
@@ -0,0 +1,17 @@
+// Copyright © 2018 Dell Inc. or its subsidiaries. All Rights Reserved. 
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-task-data-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/task-data/base-tasks/wsman-update-lookups-base.js');
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
+
---
+++ spec/lib/task-data/tasks/wsman-update-lookups-spec.js
@@ -0,0 +1,17 @@
+// Copyright © 2018 Dell Inc. or its subsidiaries. All Rights Reserved. 
+
+'use strict';
+
+describe(require('path').basename(__filename), function () {
+    var base = require('./base-tasks-spec');
+
+    base.before(function (context) {
+        context.taskdefinition = helper.require('/lib/task-data/tasks/wsman-update-lookups.js');
+    });
+
+    describe('task-data', function () {
+        base.examples();
+    });
+
+});
+
